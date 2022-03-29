8ba45f553d50474b489cd4f387faac3c3ae06955
Samuel Blackburn <samuel.blackburn@nbrown.co.uk>
2022-03-29 14:25:24 +0100

    Initial Commit

---
+++ SwiftUI DatePicker/SwiftUI_DatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DatePickerApp.swift
+//  SwiftUI DatePicker
+//
+//  Created by Samuel Blackburn on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
